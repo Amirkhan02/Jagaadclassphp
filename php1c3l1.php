d6e9541ac16eff194e5adf1f29e86b6eb8a8c2e8
Hamme Shittu <khanshittu50@gmail.com>
2022-09-30 13:08:39 -0100

    halfdone assignmentk

---
+++ php1c3l1.php
@@ -0,0 +1,14 @@
+<?php
+$input = readLine("What's the number of text you want to reverse ?: ");
+
+$length1 =  $input;
+
+if ($length1 = $input){
+    $string1 = readline("Enter a text:");
+    $length2 = strlen($string1);
+} for ($i1=($length2 -1) ; $i1 >=0 ; $i1--)
+{
+    echo $string1[$i1];
+}
+
+

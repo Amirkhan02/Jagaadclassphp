7034fdc20e09208815ef403acb3b8663b9881cb5
Hamme Shittu <khanshittu50@gmail.com>
2022-10-04 10:43:58 -0100

    Yet to be comlpeted

--- exercisec2.php
+++ exercisec2.php
@@ -1,13 +1,58 @@
 <?php
-$input = readline('Enter numbers in word:');
-$input = array("Zero" =>"0", "One" =>"1",  "Two" =>"2", "Three" =>"3", "Four" =>"4", "Five" =>"5", "Six" =>"6", "Seven" =>"7", "Eight" =>"8", "Nine" =>"9");
-print_r (explode(" ",$input()));
+$input = readline('Enter numbers in word between 0 and 9:');
+$inputArray = explode(',', $input);
+
+$inputedArray =[];
+for ($i=0; $i<count($inputedArray); $i++) {
+    switch ($inputedArray[$i]) {
+        case 'zero':
+            array_push($inputedArray, 0);
+            break;
+        case 'one':
+            array_push($inputedArray, 1);
+            break;
+        case 'two':
+            array_push($inputedArray, 2);
+            break;
+        case 'three':
+            array_push($inputedArray, 3);
+            break;
+        case 'four':
+            array_push($inputedArray, 4);
+            break;
+        case 'five':
+            array_push($inputedArray, 5);
+            break;
+        case 'six':
+            array_push($inputedArray, 6);
+            break;
+        case 'seven':
+            array_push($inputedArray, 7);
+            break;
+        case 'eight':
+            array_push($inputedArray, 8);
+            break;
+        case 'nine':
+            array_push($inputedArray, 9);
+            break;   
+        default:
+            echo 'Enter the correct value';
+            break;   
+    }
+}
+echo implode('', $convertedArray);
+
+
+
+
+
+
 
 
 /*
 //$input = $word_input;
 //print_r(explode(" ", $str, 0));
-function word_input($input){
+function wordsTodigit($inputArray){
     $input = readline('Enter numbers in word:');
 
     $iarr = explode('; ', $input);
@@ -23,7 +68,7 @@ function word_input($input){
             case 'two':
                 $result .= '2';
 
-            case 'three':
+            case 'three':   
                 $result .= '3';
 
             case 'four':
---
+++ php1c3l2.php
@@ -0,0 +1,7 @@
+<?php
+$numberOfStudent = readline("How many students does the class have?: ")
+//for ($numberOfStudent) {
+    $numberOfStudent = readline array("Student - name: \n", "Student - grade: " );
+//} 
+
+?>

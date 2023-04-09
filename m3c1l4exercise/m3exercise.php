54fec5e75555c6991e3e4b42bfa9b7614d7c1e31
Hamme Shittu <khanshittu50@gmail.com>
2023-04-09 02:21:37 -0100

    updateexercise

--- m3c1l4exercise/m3exercise.php
+++ m3c1l4exercise/m3exercise.php
@@ -1,1 +1,11 @@
 <?php
+class  Average {
+    private float $average;
+
+    public function calculate(float $num1, float $num2, float $num3): void {
+        $average = ($num1 + $num2 + $num3) / 3;
+        echo $average . PHP_EOL;
+    }
+}
+
+Average::calculate();
